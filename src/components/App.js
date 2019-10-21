9735ab9842ee00a16e7c38a2904ffd4eaec1e194
Julio García <julioggonz@gmail.com>
2019-10-20 23:40:17 -0500

    Setup Apollo

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,49 @@
 import React from "react";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql"
+});
+
+const GET_ENTRIES_QUERY = gql`
+  {
+    entries {
+      id
+      description
+      date
+      amountCents
+    }
+  }
+`;
+
+function EntriesTable() {
+  const { loading, error, data } = useQuery(GET_ENTRIES_QUERY);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error.</p>;
+  }
+
+  return (
+    <ul>
+      {data.entries.map(entry => (
+        <li key={entry.id}>
+          {entry.description} | {entry.amountCents} | {entry.date}
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default function App() {
   return (
-    <pre>
-      <code>Hello World</code>
-    </pre>
+    <ApolloProvider client={client}>
+      <EntriesTable />
+    </ApolloProvider>
   );
 }
