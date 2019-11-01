ba99628031d97d26aaab49dc9cb1fa4b65550321
Julio García <julioggonz@gmail.com>
2019-11-01 12:23:34 -0500

    Add getEntryDatePredicates function

--- src/components/EntriesTable/processEntries.js
+++ src/components/EntriesTable/processEntries.js
@@ -3,20 +3,42 @@ import { differenceInDays } from "date-fns";
 import calculateMonths from "./calculateMonths";
 
 const sortEntries = sortBy(prop("dateAsDate"));
-const addParsedDateToEntry = entry => ({
+const addDateAsDateProperty = entry => ({
   ...entry,
   dateAsDate: new Date(entry.date)
 });
 
+function getEntryDatePredicates(entries, entry, index) {
+  const nextEntry = entries[index + 1];
+  const getYear = entry => entry.dateAsDate.getUTCFullYear();
+  const getMonth = entry => entry.dateAsDate.getUTCMonth();
+
+  if (nextEntry) {
+    if (getYear(entry) !== getYear(nextEntry)) {
+      return { isLastOfYear: true, isLastOfMonth: true, isLastOfTrail: false };
+    } else if (getMonth(entry) !== getMonth(nextEntry)) {
+      return { isLastOfYear: false, isLastOfMonth: true, isLastOfTrail: false };
+    } else {
+      return {
+        isLastOfYear: false,
+        isLastOfMonth: false,
+        isLastOfTrail: false
+      };
+    }
+  } else {
+    return { isLastOfYear: true, isLastOfMonth: true, isLastOfTrail: true };
+  }
+}
+
 function processEntries(rawEntries) {
-  const entriesWithDates = rawEntries.map(addParsedDateToEntry);
+  const entriesWithDates = rawEntries.map(addDateAsDateProperty);
   const sortedEntries = sortEntries(entriesWithDates);
 
   const today = new Date();
   let total = 0;
   let todayTotal = 0;
 
-  const entries = sortedEntries.map(entry => {
+  const entries = sortedEntries.map((entry, index) => {
     total += entry.amountCents;
     if (entry.dateAsDate <= today) {
       todayTotal += entry.amountCents;
@@ -28,7 +50,8 @@ function processEntries(rawEntries) {
       date: entry.date,
       amountCents: entry.amountCents,
       totalCents: total,
-      todayCloseness: Math.abs(differenceInDays(entry.dateAsDate, today))
+      todayCloseness: Math.abs(differenceInDays(entry.dateAsDate, today)),
+      ...getEntryDatePredicates(sortedEntries, entry, index)
     };
   });
 
