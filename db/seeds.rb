71fc8de8b3148bef77a1a4a25f79f0bb2c6f418c
Julio García <julioggonz@gmail.com>
2019-11-03 01:32:41 -0500

    Fix seeds bad calculations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Entry.delete_all
 CSV.foreach("./data.csv") do |row|
   description, raw_date, raw_amount = row
   date = Date.strptime(raw_date, "%m/%d/%Y")
-  amount_cents = raw_amount.to_i * 100
+  amount_cents = (raw_amount.to_d * 100).to_i
 
   Entry.create!(
     description: description,
