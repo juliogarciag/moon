a2c73fca083730501626a054cff3a7893f248ba3
Julio García <julioggonz@gmail.com>
2019-12-21 09:37:26 -0500

    Fix seed behavior

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,14 @@ require "csv"
 EntryVersion.delete_all
 Entry.delete_all
 
+def unformat_number(text)
+  text.gsub(/\$|,/, "").strip.to_d
+end
+
 CSV.foreach("./data.csv") do |row|
   description, raw_date, raw_amount = row
   date = Date.strptime(raw_date, "%m/%d/%Y")
-  amount_cents = (raw_amount.to_d * 100).to_i
+  amount_cents = (unformat_number(raw_amount) * 100).to_i
 
   entry = Entry.create!(
     description: description,
